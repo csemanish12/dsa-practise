17b51e91338a7f3def24fe3928db565b76b6fc63
manish <sahmanish20@gmail.com>
2024-06-18 20:08:21 +0545

    create fib generator

--- python/generators.py
+++ python/generators.py
@@ -10,3 +10,16 @@ for number in sequence_generator([1, 2, 3, 4]):
 generator = (item for item in [1, 2, 3, 4])
 for number in generator:
     print("number via generator:", number)
+
+
+def fibonacci_generator(stop=10):
+    curr_fib, next_fib = 0, 1
+    for _ in range(stop):
+        fib = curr_fib
+        curr_fib, next_fib = next_fib, curr_fib + next_fib
+
+        yield fib
+
+
+for number in fibonacci_generator(15):
+    print("fib num:", number)
